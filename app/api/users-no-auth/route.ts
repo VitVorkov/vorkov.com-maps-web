a82df6c18054a06ffddc96eb344852887c1a7a87
Vitalii Vorkov <vitvorkov@gmail.com>
2023-09-06 18:39:25 +0200

    Attempt to fix Next Deployment - 3

--- app/api/users-no-auth/route.ts
+++ app/api/users-no-auth/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export const GET = async function GET() {
   const res = new NextResponse();
 
-  const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/users/no-auth`;
+  const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/users/no-auth/`;
   const response = await fetch(url, {
     cache: "no-store",
   });
