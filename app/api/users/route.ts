dadb515269a8d5f327d88076f3dff98b8f64b83b
Vitalii Vorkov <vitvorkov@gmail.com>
2023-09-06 19:10:56 +0200

    Attempt to fix Next Deployment - 9

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -5,17 +5,8 @@ export const GET = withApiAuthRequired(async function GET(req) {
   const res = new NextResponse();
   const { accessToken } = await getAccessToken(req, res);
 
-  const url = `${
-    process.env.NEXT_PUBLIC_BACKEND_URL
-  }/users?email=${req.nextUrl.searchParams.get("email")}`;
-  console.log(url);
-  const response = await fetch(url, {
-    cache: "no-store",
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
-  const user = await response.json();
-  console.log(user);
-  return NextResponse.json(user, { status: 200 });
+  const url = "https://jsonplaceholder.typicode.com/users";
+  const response = await fetch(url);
+  const responseJson = await response.json();
+  return NextResponse.json(responseJson, { status: 200 });
 });
